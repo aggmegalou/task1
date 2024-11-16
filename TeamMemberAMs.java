03eaafc30eb76cafb82cfbb8401be408456c496e
Valeria Soumelidi <t8230135@aueb.gr>
2024-11-16 16:08:48 +0200

    Προσθήκη του ΑΜ της Βαλέριας Σουμελίδη

--- TeamMemberAMs.java
+++ TeamMemberAMs.java
@@ -3,6 +3,7 @@ public class TeamMemberAMs {
     public static void main(String[] args) {
 
         System.out.println("AM Άννα Μαρία Μεγάλου: t8230094");
+        System.out.println("ΑΜ Βαλέρια Σουμελίδη: t8230135");
  
     } 
 } 
