727f9b3f86d15b4a7890d0cd8955a4c4767067dd
Nellyandr <t8230007@aueb.gr>
2024-11-16 19:10:10 +0200

    Προσθεσα ΑΜ 8230007

--- TeamMemberAMs.java
+++ TeamMemberAMs.java
@@ -6,6 +6,7 @@ public class TeamMemberAMs {
         System.out.println("ΑΜ Βαλέρια Σουμελίδη: t8230135");
         System.out.println("ΑΜ Μαρία Σπάχου: t8230136");
         System.out.println("ΑΜ Κωνσταντία Νικολέτα Στεργίου: t8230144");
+        System.out.println("ΑΜ Ευαγγελία Νεκταρία Ανδρουλάκη: t8230007");
 
  
     } 
